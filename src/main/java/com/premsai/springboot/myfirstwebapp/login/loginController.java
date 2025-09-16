759463d9b97fa7e52fa7b904ab6451fc4f7886f9
premsai-20 <sakamurisai252@gmail.com>
2025-09-16 11:23:55 +0530

    1. New login jsp has been created

---
+++ src/main/java/com/premsai/springboot/myfirstwebapp/login/loginController.java
@@ -0,0 +1,14 @@
+package com.premsai.springboot.myfirstwebapp.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class loginController {
+	
+	
+	@RequestMapping("/login")
+	public String gotoLoginPage() {
+		return "login";
+	}
+}
---
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/sayHello.jsp
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>JSP </title>
+</head>
+<body>
+    <!-- Your content goes here -->
+    HELLOW FROM JSP
+</body>
+</html>
