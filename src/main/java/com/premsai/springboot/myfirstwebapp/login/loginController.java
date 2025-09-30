121db2f22a2d3a44fd3642237b653e2a6b8874d1
premsai-20 <sakamurisai252@gmail.com>
2025-09-30 06:19:16 +0530

    1. Authentication

--- src/main/java/com/premsai/springboot/myfirstwebapp/login/loginController.java
+++ src/main/java/com/premsai/springboot/myfirstwebapp/login/loginController.java
@@ -3,17 +3,44 @@ package com.premsai.springboot.myfirstwebapp.login;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class loginController {
 	
 	
-	@RequestMapping("/login")
-	public String gotoLoginPage(@RequestParam String name, ModelMap model) {
-		model.put("name", name);
-		System.out.println("Request param is " + name); 
+	public loginController(AuthenticationService authenticationService) {
+		super();
+		this.authenticationService = authenticationService;
+	}
+
+	private AuthenticationService authenticationService ;
+	
+	@RequestMapping(value="/login", method = RequestMethod.GET)
+	public String gotoLoginPage() {
 		
 		return "login";
+	}
+	
+	@RequestMapping(value="/login", method = RequestMethod.POST)
+	public String gotoWelcomPage(@RequestParam String name,
+								 @RequestParam String password,
+								 ModelMap model) {
+		if(authenticationService.authenticate(name, password)) {
+		
+			model.put("name", name);
+			model.put("password", password);
+			//authentication
+			//name= premsai 
+			//password= dummy
+			
+			return "welcomelogin";
+		}
+		
+		model.put("errorMessage", "Invalid Credentials! Please try again" );
+		return "login";
+		
 	}
 }
--- src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
@@ -6,6 +6,12 @@
     <title>LOGIN PAGE</title>
 </head>
 <body>
-    Welcome to the login page ${name}!
+    Welcome to the login page!
+    <pre>${errorMessage} </pre>
+    <form method="post">
+    	Name: <input type="text" name="name">
+    	Password: <input type="password" name="password">
+    	<input type="submit">
+    </form>
 </body>
 </html>
